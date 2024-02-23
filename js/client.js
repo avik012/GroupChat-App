2a3b5dc63cd1c38a5be32819495a11d1142e04e2
avik012 <Avinashkumawat12@gmail.com>
2024-02-23 14:56:20 +0530

    add remote web socket to html

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000')
+const socket = io('https://groupchat-app.onrender.com')
 
 const messageContainer = document.getElementById('chatContainer')
 const messageInput = document.getElementById('inputMsg')
